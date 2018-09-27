594065fd60f33418d9e47c1ecb299da3c0874cc5
Александр <adsl41c@gmail.com>
2018-09-27 02:57:23 +0000

    Google drive

--- index.php
+++ index.php
@@ -1,46 +1,11 @@
-<?php include_once 'server-info.php'?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-    <title>Starter Template for Bootstrap</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  </head>
-  <body>
-    <main role="main" class="container">
-      <h1>Server & file info</h1>
-      <?php if($server):?>
-        <ul class="list-group">
-          <?php foreach($server as $key => $value):?>
-            <li class="list-group-item">
-              <strong><?=$key?>: </strong>
-              <?=$value?>
-            </li>
-          <?php endforeach ?>
-        </ul>
-      <?php endif;?>
-      <h1>Server & file info</h1>
-      <?php if($server):?>
-        <ul class="list-group">
-          <?php foreach($client as $key => $value):?>
-            <li class="list-group-item">
-              <strong><?=$key?>: </strong>
-              <?=$value?>
-            </li>
-          <?php endforeach ?>
-        </ul>
-      <?php endif;?>
-    </main><!-- /.container -->
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-</html>
+Client ID
+606012579961-pd2mf70qjd2gs2vtm0c25r5t5ug6n1br.apps.googleusercontent.com
+
+Client Secret
+AGvuQo7FOnYzE9A9KFiKNyKy
+
+606012579961-jh75me3kkud2s36p9beg8csggh4rkb0i.apps.googleusercontent.com
+
+76A6vt3QQX2h7vK-4f1xMe10
+
+4/ZgCoDJIJgdUAfihvkrDtG_hfHqheWuaUCvmaTVPmiTbCFVbF96z7YL4
