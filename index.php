d5f9e32005f6fe0c2c02f9e5bab123a252bbd262
Александр <adsl41c@gmail.com>
2018-10-01 10:52:50 +0000

    add error log

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
 // PHP Errors
 ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+ini_set('php_flag html_errors on', true);
+ini_set('log_errors', false);
+ini_set('error_log', __DIR__);
+
+// PHP Errors Class Logger
+require_once(__DIR__.'/application/modules/logger.php');
+define('LOGGER', true); // Logger
+define('LOGGER_PRINT', true); // Logger echo
+define('LOGGER_PATH', __DIR__); // Logger path
+
 // Default template
 define('TEMPLATE', 'template.php');
 
 /**
 Module:
 */
-
-// Logger
-require_once(__DIR__.'/application/modules/logger.php');
-define('LOGGER', true); // Logger
-define('LOGGER_PATH', __DIR__); // Logger path
-define('LOGGER_PRINT', true); // Logger echo
 
 // Google Drive vendor
 require_once __DIR__.'/application/modules/google/api.php';
